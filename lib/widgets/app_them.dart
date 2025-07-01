0de0e1629300abf389e50cb468cd32315e295338
Youssef Ibrahim <youssefabrahem6@gmail.com>
2025-07-01 03:08:27 +0300

    message

--- lib/widgets/app_them.dart
+++ lib/widgets/app_them.dart
@@ -4,11 +4,10 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 const Color primaryColor = Color(0xFF8B0000); // Dark red
-const Color accentColor =  Color(0xFFD4AF37); // Gold accent
-const Color backgroundColor =  Color(0xFFF5F5F5); // Light gray background
+const Color accentColor = Color(0xFFD4AF37); // Gold accent
+const Color backgroundColor = Color(0xFFF5F5F5); // Light gray background
 const Color cardColor = Colors.white;
-const Color textColor =  Color(0xFF333333);
-const Color secondaryTextColor =  Color(0xFF666666);
-const Color highlightColor =  Color(0xFFFFD700);
-const Color secondaryColor =  Color(0xFFF5F5DC);
-  
+const Color textColor = Color(0xFF333333);
+const Color secondaryTextColor = Color(0xFF666666);
+const Color highlightColor = Color(0xFFFFD700);
+const Color secondaryColor = Color(0xFFF5F5DC);
